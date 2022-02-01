baeeeede2ead32c13ced531dd638b9ee69767d75
Sazonov Nikita <actpohabt.ns@gmail.com>
2022-02-01 18:41:58 +0200

    [ref] migrate from ganache to hardhat network

--- contracts/README.md
+++ contracts/README.md
@@ -2,6 +2,6 @@
 
 ## Deploy NitroAdjudicator
 
-1. In first terminal run `ganache-cli -s yellow`
+1. In first terminal run `npx hardhat node`
 2. In second run `npx hardhat run scripts/deployAdjudicator.ts --network localhost`
 3. Get NitroAdjudicator address from console
--- contracts/scripts/deployAdjudicator.ts
+++ contracts/scripts/deployAdjudicator.ts
@@ -1,9 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  let privateKey = '0x9a14bf0eb618a3407a12a83a74dfe7bbed098ccc6347985b92ab08e81996cfc9';
-  let provider = new ethers.providers.JsonRpcProvider();
-  let deployer = new ethers.Wallet(privateKey, provider);
+  const [deployer] = await ethers.getSigners();
   
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
